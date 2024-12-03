8a6ff046516f6f515bb3fc6a02cf08ee3dd22834
redssy <redlemagic@hotmail.com>
2024-12-02 22:37:16 -0300

    prd vs dev

--- src/servicos/api.ts
+++ src/servicos/api.ts
@@ -6,7 +6,7 @@ import { Competencia } from '../globais'
 // conexao com servidor arquivo .json
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.API_BASE_URL || 'http://localhost:4000'
+    baseUrl: process.env.API_BASE_URL
   }),
   endpoints: (builder) => ({
     getCompetencias: builder.query<Competencia[], void>({
